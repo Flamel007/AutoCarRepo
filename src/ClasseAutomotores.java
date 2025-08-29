bcd7a724d016fd934df3112506390493ee04ceb6
Flamel007 <flamelblackdeath007@gmail.com>
2025-08-29 10:18:29 -0300

    Adicionado getters e setters

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -1,4 +1,17 @@
 
 public class ClasseAutomotores {
-
+	String corAt;
+	String marcaAt;
+	String modeloAt;
+	String tipoAt;
+	
+	// Construtor vazio 
+	public ClasseAutomotores() {
+		
+	}
+	
+	// Construtor vazio 
+	public ClasseAutomotores() {
+		
+	}
 }
