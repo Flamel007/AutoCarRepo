be7b974c0523f53a8bec605deeeaa11529c78b3c
Flamel007 <flamelblackdeath007@gmail.com>
2025-09-05 10:11:27 -0300

    Adicionada classe motos herdando atributos e métodos da classe

---
+++ src/ClasseMotos.java
@@ -0,0 +1,7 @@
+
+public class ClasseMotos extends ClasseAutomotores {
+
+	public ClasseMotos(String corPar, String marcaPar, String modeloPar, String tipoPar) {
+		super(corPar, marcaPar, modeloPar, tipoPar);
+}
+}
